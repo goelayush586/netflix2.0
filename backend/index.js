43ea55b6d3775af8b645bb9e08b29a5539dba3eb
goelayush <goelayush586@gmail.com>
2024-09-09 20:52:15 +0530

    production ready app

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+//step-1
+// const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+import databaseConnection from "./utils/database.js";
+import cookieParser from "cookie-parser";
+import userRoute from "./routes/userRoute.js";
+import cors from "cors";
+import path from "path";
+// databaseConnection();
+
+// dotenv.config({
+//     path:".env"
+// })
+dotenv.config({ path: ".env" });
+databaseConnection();
+
+
+const app = express();
+const _dirname=path.resolve();
+//middlewares 
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(cookieParser());
+const corsOptions = {
+    origin:'http://localhost:3000',
+    credentials:true
+}
+app.use(cors(corsOptions));
+ 
+// api
+app.use("/api/v1/user", userRoute);
+
+app.use(express.static(path.join(_dirname,"/netflix/build")));
+app.get('*',(_,res)=>{
+    res.sendFile(path.resolve(_dirname,"netflix","build","index.html"));
+})
+app.listen(process.env.PORT,() => {
+    console.log(`Server listen at port ${process.env.PORT}`);
+});
